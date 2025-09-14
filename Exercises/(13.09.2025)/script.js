1a5b37cd2d53e8f4ba6c86e4669956c112868600
Priyadharshini D <priyabio005@gmail.com>
2025-09-14 18:52:56 +0530

    Day1 Exercise

---
+++ Exercises/(13.09.2025)/script.js
@@ -0,0 +1,71 @@
+/*// CHALLENGE 1:
+//TEST DATA 1:
+
+let MassOfMark1 = 78; 
+let HeightOfMark1 = 1.69; 
+let MassOfJohn1 = 92;  
+let HeightOfJohn1 = 1.95; 
+
+let MarkBMI1 = MassOfMark1 / (HeightOfMark1 ** 2);   // BMI = mass / height ** 2
+let JohnBMI1 = MassOfJohn1 / (HeightOfJohn1 * HeightOfJohn1); // BMI = mass / (height * height)
+
+let MarkHigherBMI1 = MarkBMI1 > JohnBMI1;
+console.log("Does Mark have a higher BMI than John?", MarkHigherBMI1);
+
+if (MarkBMI1 > JohnBMI1) {
+  console.log(`Mark's BMI is higher than John's!`);
+} else {
+  console.log(`John's BMI is higher than Mark's!`);
+}
+//TEST DATA 2:
+
+let MassOfMark2 = 95; 
+let HeightOfMark2 = 1.88; 
+let MassOfJohn2 = 85;  
+let HeightOfJohn2 = 1.76; 
+
+let MarkBMI2 = MassOfMark2 / (HeightOfMark2 ** 2);   // BMI = mass / height ** 2
+let JohnBMI2 = MassOfJohn2 / (HeightOfJohn2 * HeightOfJohn2); // BMI = mass / (height * height)
+
+let MarkHigherBMI2= MarkBMI2 > JohnBMI2;
+console.log("Does Mark have a higher BMI than John?", MarkHigherBMI2);
+*/
+
+
+//CHALLENGE 2:
+//TEST DATA 1:
+
+let MassOfMark1 = 78; 
+let HeightOfMark1 = 1.69; 
+let MassOfJohn1 = 92;  
+let HeightOfJohn1 = 1.95; 
+
+let MarkBMI1 = MassOfMark1 / (HeightOfMark1 ** 2);   // BMI = mass / height ** 2
+let JohnBMI1 = MassOfJohn1 / (HeightOfJohn1 * HeightOfJohn1); // BMI = mass / (height * height)
+
+let MarkHigherBMI1 = MarkBMI1 > JohnBMI1;
+console.log("Does Mark have a higher BMI than John?", MarkHigherBMI1);
+
+if (MarkBMI1 > JohnBMI1) {
+  console.log(`Mark's BMI is higher than John's!`);
+} else {
+  console.log(`John's BMI is higher than Mark's!`);
+}
+//TEST DATA 2:
+
+let MassOfMark2 = 95; 
+let HeightOfMark2 = 1.88; 
+let MassOfJohn2 = 85;  
+let HeightOfJohn2 = 1.76; 
+
+let MarkBMI2 = MassOfMark2 / (HeightOfMark2 ** 2);   // BMI = mass / height ** 2
+let JohnBMI2 = MassOfJohn2 / (HeightOfJohn2 * HeightOfJohn2); // BMI = mass / (height * height)
+
+let MarkHigherBMI2= MarkBMI2 > JohnBMI2;
+console.log("Does Mark have a higher BMI than John?", MarkHigherBMI2);
+
+if (MarkBMI2 > JohnBMI2) {
+  console.log(`Mark's BMI is higher than John's!`);
+} else {
+  console.log(`John's BMI is higher than Mark's!`);
+}
